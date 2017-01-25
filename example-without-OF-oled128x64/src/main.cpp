8854c76ca468054c83abc36adee75c9cad3954a7
kashimAstro <d.longobardi@ziggurats.net>
2017-01-25 18:06:41 +0000

    Update oled function

--- example-without-OF-oled128x64/src/main.cpp
+++ example-without-OF-oled128x64/src/main.cpp
@@ -1,24 +1,71 @@
 #include <iostream>
 #include "ofxGPIO.h"
 #include "font.h"
+#include "bitmap.h"
+
 #define OLED 0x3C
 #define WIDTH_OLED 128
 #define HEIGHT_OLED 64
+#define COMMAND_BLACK_BACKGROUND 0xA6
+#define COMMAND_WHITE_BACKGROUND 0xA7
+#define COMMAND_MIRROR_VERTICAL 0xA0 | 0x1
+#define COMMAND_MIRROR_HORIZONTAL 0xC8
+#define PAGE_ADDRESSING 0x02
+
 #define DATA 0x40
 #define CMD  0x80
 
-void clearDisplay(I2CBus * b)
+static I2CBus * bus;
+
+void setPageMode()
+{
+        bus->writeByte(CMD,0x20);
+        bus->writeByte(CMD,PAGE_ADDRESSING);
+}
+
+void rotateDisplay180()
+{
+        bus->writeByte(CMD,COMMAND_MIRROR_VERTICAL);
+        bus->writeByte(CMD,COMMAND_MIRROR_HORIZONTAL);
+}
+
+void setWhiteBackground()
+{
+        bus->writeByte(CMD,COMMAND_WHITE_BACKGROUND);
+}
+
+void setBlackBackground()
+{
+        bus->writeByte(CMD,COMMAND_BLACK_BACKGROUND);
+}
+
+void setCursor(int x, int y)
+{
+	bus->writeByte(CMD,0x00 + ( 8 * x & 0x0F));
+	bus->writeByte(CMD,0x10 + ((8 * x >> 4) & 0x0F));
+        bus->writeByte(CMD,0xB0 + y);
+}
+
+void drawBitmap(int *Bitmaparray, int Width=WIDTH_OLED, int Height=HEIGHT_OLED)
+{
+	for(int i = 0; i < Width * 8 * Height; i++)
+	{
+		bus->writeByte(DATA,Bitmaparray[i]);
+	}
+}
+
+void clearDisplay()
 {
 	for(int X = 0; X < WIDTH_OLED; X++)
 	{
 		for(int Y = 0; Y < HEIGHT_OLED; Y++) 
 		{
-			b->writeByte(DATA,0);
+			bus->writeByte(DATA,0);
 		}
 	}
 }
 
-void printChar(I2CBus * b, char Char)
+void printChar(char Char)
 {
     	if (Char < 32 || Char > 127)
         {
@@ -26,17 +73,45 @@ void printChar(I2CBus * b, char Char)
         }
 	for(int i = 0; i < 8; i++)
 	{
-	       b->writeByte(DATA,OLEDFont[Char - 32][i]);
+	       bus->writeByte(DATA,OLEDFont[Char - 32][i]);
     	}
 }
 
-void printString(I2CBus * b, const char *Data)
+void printString(char *Data)
 {
 	int i = 0;
         while(Data[i] && i < WIDTH_OLED)
         {
-		printChar(b,Data[i++]);
+		printChar(Data[i++]);
 	}
+}
+
+void printNumber(long Data)
+{
+        char Buffer[10] = "";
+        int IntPart = 0;
+        int FloatPart = 0;
+        if (Data < 0)
+        {
+                FloatPart++;
+                printChar('-');
+                Data = -Data;
+        }
+        else if (Data == 0)
+        {
+                FloatPart++;
+                printChar('0');
+        }
+        while (Data > 0)
+        {
+                Buffer[IntPart++] = Data % 10;
+                Data /= 10;
+        }
+        FloatPart += IntPart;
+        for(; IntPart > 0; IntPart--)
+        {
+                printChar('0' + Buffer[IntPart - 1]);
+        }
 }
 
 int main(int argc, char *argv[])
@@ -45,9 +120,10 @@ int main(int argc, char *argv[])
 	{
 		srand (time(NULL));
 
-        	I2CBus * bus;
 		bus = new I2CBus("/dev/i2c-1");
        		bus->addressSet(OLED);
+		setWhiteBackground();
+
 		string ar = argv[1];
 		if(ar == "rand")
 		{
@@ -62,11 +138,30 @@ int main(int argc, char *argv[])
 		}
 		if(ar == "clear")
 		{
-			clearDisplay(bus);
+			clearDisplay();
 		}
 		if(ar == "letter")
 		{
-			printString(bus,"Openframeworks");			
+			setCursor(0,0);
+			printString("OF ofxGPIO");
+		}
+		if(ar == "bitmap")
+		{
+			setCursor(0,0);
+			drawBitmap(Bitmap,11,11);
+		}
+		if(ar == "bgblack")
+		{
+	                setBlackBackground();
+		}
+		if(ar == "rotate")
+		{
+			rotateDisplay180();
+		}
+		if(ar == "number")
+		{
+			setCursor(0,0);
+			printNumber(300234);
 		}
 	}
 	else{
