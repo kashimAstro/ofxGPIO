a44d4fe0fe823d016f7a675fc3c1b858be3285a8
Pascal Baltazar <pascal@baltazars.org>
2019-05-26 12:43:32 +0200

    fixing palette generation

--- example-apa102-ledstrip/src/main.cpp
+++ example-apa102-ledstrip/src/main.cpp
@@ -9,30 +9,41 @@ class ofApp : public ofBaseApp
 		vector<ofVec3f> colors;
 		int numLed;
 		int brightness;
+    
+        void create_palett()
+        {
+            for(int i = 0; i < numLed; i++) {
+                colors.push_back(ofVec3f(
+                                         (int)ofRandom(0,255),
+                                         (int)ofRandom(0,255),
+                                         (int)ofRandom(0,255)
+                                         ));
+            }
+        }
 
-		void gen_palett()
-		{
-			for(int i = 0; i < numLed; i++) {
-					colors.push_back(ofVec3f(
-						(int)ofRandom(0,255),
-						(int)ofRandom(0,255),
-						(int)ofRandom(0,255)
-					));
-			}
-		}
+        void gen_palett()
+        {
+            for(int i = 0; i < numLed; i++) {
+                colors[i] = ofVec3f(
+                              (int)ofRandom(0,255),
+                              (int)ofRandom(0,255),
+                              (int)ofRandom(0,255)
+                              );
+            }
+        }
 
 		void setup()
 		{
 			brightness = 5;
-			numLed     = 60;
-			gen_palett();
+			numLed     = 95;
+			create_palett();
 			apa.setupAPA102();
 		}
 
 		void update(){
 			gen_palett();
 			apa.setAPA102(numLed,colors,5);
-			usleep(5000);
+			usleep(1000);
 		}
 		
 		void exit(){
